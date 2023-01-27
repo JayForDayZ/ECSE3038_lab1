21bec9fd068bbb257679ec032aa880b0bf633898
JayForDayZ <110322491+JayForDayZ@users.noreply.github.com>
2023-01-27 16:56:42 -0500

    commit

--- LAB1.py
+++
@@ -1,9 +0,0 @@
-def parallel(resistors):
-    if not resistors or len(resistors)<2:
-        return "Error!!"
-
-    reciprocal = sum([1/res for res in resistors])
-    resistance = 1/reciprocal
-    return resistance
-    
-    print(parallel([330,400,200]))
